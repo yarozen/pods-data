410568a18eff7bc56028d5799c414fef979a4bbf
Yaniv Rozenboim <yarozen@gmail.com>
2022-06-18 23:08:20 +0300

    test3

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-import kubernetes
-import time
+from kubernetes import client, config
+
+config.incluster_config()
+v1 = client.CoreV1Api()
 
 while True:
-    print("test2")
-    time.sleep(10)
+    print("Listing pods with their IPs:")
+    ret = v1.list_pod_for_all_namespaces(watch=False)
+    for i in ret.items:
+        print("%s\t%s\t%s" %
+              (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
